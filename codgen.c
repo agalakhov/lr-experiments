f32340afd04d3acefc4b912c5ffe771b4dab8751
Alexey Galakhov <alexey.galakhov@ise.de>
2014-11-14 15:20:58 +0100

    Refactor codgen

--- codgen.c
+++ codgen.c
@@ -3,57 +3,93 @@
 #include "grammar_i.h"
 #include "lr0_i.h"
 
+#include <string.h>
+
+struct argument {
+    signed          stack_index;
+    const char *    host_type;
+    const char *    name;
+};
+
+struct reduce {
+    const char *    name;
+    const char *    host_code;
+    unsigned        nargs;
+    struct argument args[];
+};
+static inline size_t sizeof_struct_reduce(unsigned nargs) {
+    return sizeof(struct reduce) + nargs * sizeof(struct argument);
+}
+
+typedef void (*emit_func_t)(FILE *fd, const struct reduce *reduce);
+
 static const char*
 symtype(const struct symbol *sym)
 {
     return sym->host_type ? sym->host_type : "void *";
 }
 
-void
-codgen_c(FILE *fd, lr0_machine_t machine)
-{
-    fprintf(fd, "/* GENERATED FILE - DO NOT EDIT */\n\n");
 
+static void
+foreach_rule(lr0_machine_t machine, emit_func_t func, FILE *fd)
+{
     const struct grammar *grammar = machine->grammar;
     for (const struct symbol *sym = grammar->symlist.first; sym; sym = sym->next) {
         if (sym->type != NONTERMINAL)
             continue;
         for (const struct rule *rule = sym->nt.rules; rule; rule = rule->next) {
-            if (rule->host_code) {
-                if (rule->ls_label) {
-                    fprintf(fd, "#define %s (*__%s)\n", rule->ls_label, rule->ls_label);
-                }
-                for (unsigned i = 0; i < rule->length; ++i) {
-                    if (rule->rs[i].label) {
-                        fprintf(fd, "#define %s (*__%s)\n", rule->rs[i].label, rule->rs[i].label);
-                    }
-                }
-                fprintf(fd, "static void __reduce_%s_%u (", sym->name, rule->id);
-                unsigned irule = 0;
-                if (rule->ls_label) {
-                    fprintf(fd, "\n  %s *__%s", symtype(sym), rule->ls_label);
-                    ++irule;
-                }
-                for (unsigned i = 0; i < rule->length; ++i) {
-                    if (rule->rs[i].label) {
-                        fprintf(fd, "%s\n  %s const *__%s", irule ? "," : "",
-                                symtype(rule->rs[i].sym.sym),
-                                rule->rs[i].label);
-                        ++irule;
-                    }
-                }
-                fprintf(fd, "%s) {%s}\n", irule ? "\n" : "", rule->host_code);
-                if (rule->ls_label) {
-                    fprintf(fd, "#undef %s\n", rule->ls_label);
-                }
-                for (unsigned i = 0; i < rule->length; ++i) {
-                    if (rule->rs[i].label) {
-                        fprintf(fd, "#undef  %s\n", rule->rs[i].label);
-                    }
-                }
-                fprintf(fd, "\n");
+            char pool[sizeof_struct_reduce(rule->length + 1)];
+            memset(pool, 0, sizeof(pool));
+            struct reduce *reduce = (struct reduce*)pool;
+            size_t namelen = 1 + snprintf(NULL, 0, "%s_%u", rule->sym->name, rule->id);
+            char name[namelen];
+            snprintf(name, namelen, "%s_%u", rule->sym->name, rule->id);
+            reduce->name = name;
+            reduce->host_code = rule->host_code;
+            reduce->nargs = 0;
+            if (rule->ls_label) {
+                struct argument *arg = &reduce->args[reduce->nargs++];
+                arg->stack_index = 0; // FIXME
+                arg->host_type = symtype(rule->sym);
+                arg->name = rule->ls_label;
             }
+            for (unsigned i = 0; i < rule->length; ++i) {
+                if (rule->rs[i].label) {
+                    struct argument *arg = &reduce->args[reduce->nargs++];
+                    arg->stack_index = i - rule->length;
+                    arg->host_type = symtype(rule->rs[i].sym.sym);
+                    arg->name= rule->rs[i].label;
+                }
+            }
+            func(fd, reduce);
         }
     }
+}
 
+static void
+emit_reduce_c(FILE *fd, const struct reduce *reduce)
+{
+    if (! reduce->host_code)
+        return;
+    for (unsigned i = 0; i < reduce->nargs; ++i) {
+        fprintf(fd, "#define %s (*__%s)\n", reduce->args[i].name, reduce->args[i].name);
+    }
+    fprintf(fd, "static void __reduce_%s (", reduce->name);
+    for (unsigned i = 0; i < reduce->nargs; ++i) {
+        fprintf(fd, "\n  %s %s*__%s", reduce->args[i].host_type,
+                (i ? "const " : ""), reduce->args[i].name);
+    }
+    fprintf(fd, "%s) {%s}\n", (reduce->nargs ? "\n" : ""), reduce->host_code);
+    for (unsigned i = 0; i < reduce->nargs; ++i) {
+        fprintf(fd, "#undef %s\n", reduce->args[i].name);
+    }
+    fprintf(fd, "\n");
+}
+
+
+void
+codgen_c(FILE *fd, lr0_machine_t machine)
+{
+    fprintf(fd, "/* GENERATED FILE - DO NOT EDIT */\n\n");
+    foreach_rule(machine, emit_reduce_c, fd);
 }
