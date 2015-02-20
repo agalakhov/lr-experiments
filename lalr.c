a4a2ccc1cdb24dfcaf0b582a1fee72de804d557a
Alexey Galakhov <agalakhov@gmail.com>
2015-02-20 17:01:07 +0100

    Lookup includes

--- lalr.c
+++ lalr.c
@@ -13,8 +13,30 @@
 struct trans {
     const struct lr0_state *    state1;
     const struct lr0_state *    state2;
+    const struct includes *     includes;
     bitset_t                    set;
 };
+
+struct includes {
+    const struct includes *     next;
+    const struct trans *        trans;
+};
+
+static signed
+cmp_trans(const void *a, const void *b)
+{
+    const struct trans * ap = (const struct trans *) a;
+    const struct trans * bp = (const struct trans *) b;
+    if (ap->state1->id < bp->state1->id)
+        return -1;
+    if (ap->state1->id > bp->state1->id)
+        return +1;
+    if (ap->state2->id < bp->state2->id)
+        return -1;
+    if (ap->state2->id > bp->state2->id)
+        return +1;
+    return 0;
+}
 
 static bitset_t
 create_dr(lr0_machine_t lr0_machine, const struct lr0_gototab * gototab)
@@ -29,6 +51,12 @@ create_dr(lr0_machine_t lr0_machine, const struct lr0_gototab * gototab)
         }
     }
     return dr;
+}
+
+static const struct trans *
+lookup_transition(const struct trans trans[], unsigned ntrans, const struct trans * needle)
+{
+    return bsearch(needle, trans, ntrans, sizeof(trans[0]), cmp_trans);
 }
 
 static unsigned
@@ -50,6 +78,8 @@ find_transitions(lr0_machine_t lr0_machine, struct trans trans[], unsigned ntran
             }
         }
     }
+    if (trans)
+        qsort(trans, itrans, sizeof(trans[0]), cmp_trans);
     return itrans;
 }
 
@@ -90,10 +120,9 @@ find_lookback_includes(lr0_machine_t lr0_machine, const struct trans trans[], un
                 assert(st != NULL);
                 print(" (%u)", st->id);
                 if ((i + 1 >= rule->nnl) && (rule->rs[i].sym.sym->type == NONTERMINAL)) {
-                    struct trans it;
-                    it.state1 = oldst;
-                    it.state2 = st;
-                    add_includes(&it, tr);
+                    struct trans it = { .state1 = oldst, .state2 = st };
+                    const struct trans * ltr = lookup_transition(trans, ntrans, &it);
+                    add_includes(ltr, tr);
                 }
             }
             print("\n");
