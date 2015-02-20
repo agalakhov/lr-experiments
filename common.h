1f6d2e3cb9c3f2b053ee18eb1486b2b0456f261c
Alexey Galakhov <agalakhov@gmail.com>
2015-02-21 00:48:19 +0100

    Add field_of

--- common.h
+++ common.h
@@ -5,3 +5,5 @@
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 #define container_of(ptr, type, member) ((type *)((char *)(ptr) - offsetof(type, member)))
+
+#define field_of(ptr, type, member) (*(type *)((char *)(ptr) + (member)))
