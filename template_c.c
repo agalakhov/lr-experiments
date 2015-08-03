4be4f23bf5b4991635fd88473079266b68973403
Alexey Galakhov <agalakhov@gmail.com>
2015-08-03 17:29:15 +0200

    partial SR support

--- template_c.c
+++ template_c.c
@@ -4,6 +4,18 @@
 
 #include <stdlib.h>
 
+static inline unsigned
+__action(unsigned state, unsigned token)
+{
+    return __actions[state][token];
+}
+
+static inline unsigned
+__goto(unsigned state, unsigned token)
+{
+    return __gotos[state][token];
+}
+
 struct __record {
     unsigned state;
     union {
@@ -33,13 +45,31 @@ __pop(struct __stack *stack, unsigned count, unsigned symbol)
 {
     stack->sp[-count] = stack->sp[0];
     stack->sp -= count;
-    // TODO stack->sp[0].state = goto[ stack->sp[-1].state, symbol ]
+    stack->sp[0].state = __goto(stack->sp[-1].state, symbol);
     stack->sp += 1;
 }
 
-static void
-__reduce (struct __stack *stack, unsigned id) {
+static inline void
+__shift(struct stack *stack, unsigned symbol, terminal_t terminal, unsigned state)
+{
+    __assert_stack(stack, 0);
+    stack->sp[0].state = state;
+    stack->sp[0].item.__terminal = terminal;
+    stack->sp += 1;
+}
+
+static inline void
+__reduce(struct __stack *stack, unsigned id)
+{
     switch (id) {
 %%reduce
     }
 }
+
+bool
+parse(struct __stack *stack, unsigned token, terminal_t terminal)
+{
+    unsigned go = __goto(stack->sp[-1].state, token);
+
+}
+
